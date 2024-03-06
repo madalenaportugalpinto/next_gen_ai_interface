e70e377223836bb4fdc6327a969022effcec12ea
matheus ingani <matheusingani@hotmail.com>
2024-03-06 15:04:05 +0000

    examples_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :templates
+  resources :templates do
+    resources :examples, only: [:new,:create]
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
