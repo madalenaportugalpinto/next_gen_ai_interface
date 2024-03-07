7bf08b0c97ba873f44a6b4a4041a76f808bf0220
Renato Schneeberger <renato.schneeberger@iam-lab.ch>
2024-03-07 19:11:43 +0000

    changed layout on output and added better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,21 @@
 #   end
 
 puts "Cleaning database..."
+InputField.destroy_all
+ExampleField.destroy_all
+Output.destroy_all
+Example.destroy_all
 Template.destroy_all
 
+
+
 puts "Creating template..."
-luca = {title: "Luca", description: "First template", user_id: User.last.id}
-tommy = {title: "Tommy", description: "Second template", user_id: User.last.id}
+luca = {title: "My monthly report", description: "This is what I need to write every month", user_id: User.last.id}
+tommy = {title: "My anual report", description: "This is what I need to write every year", user_id: User.last.id}
 
 [luca, tommy].each do |attributes|
   template = Template.create!(attributes)
+  Example.create!(content: "Equities are still driven by a strong tech rally and the S&P 500 hit several all-time highs along the way. China had a positive month breaking out of a year-long negative trend. Bond yields picked up early in the month in the US as well as in Europe, while remaining subdued over the rest of the period. Commodities ended negative, driven by a sharp decline in natural gas prices and weaker industrial metals. The USD continued to strengthen on positive sentiment, along with cyclical currencies, such as the EUR or GBP.", template_id: Template.last.id)
   puts "Created #{template.title}"
 end
 puts "Finished!"
