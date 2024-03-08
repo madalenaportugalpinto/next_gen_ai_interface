a53d2a55292bc81e4656400b1dc795307cf9da6c
shammi aktari <shammibithe@gmail.com>
2024-03-08 11:45:22 +0000

    update home v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,25 @@ Template.destroy_all
 
 
 
-puts "Creating template..."
-luca = {title: "My monthly report", description: "This is what I need to write every month", user_id: User.last.id}
-tommy = {title: "My anual report", description: "This is what I need to write every year", user_id: User.last.id}
+puts "Creating template 1 and example 1"
+template_1 = Template.create(title: "My monthly report", description: "This is what I need to write every month", user_id: User.last.id)
+Example.create(content:"Equities are still driven by a strong tech rally and the S&P 500 hit several all-time highs along the way. China had a positive month breaking out of a year-long negative trend. Bond yields picked up early in the month in the US as well as in Europe, while remaining subdued over the rest of the period. Commodities ended negative, driven by a sharp decline in natural gas prices and weaker industrial metals. The USD continued to strengthen on positive sentiment, along with cyclical currencies, such as the EUR or GBP.", template: template_1 )
 
-[luca, tommy].each do |attributes|
-  template = Template.create!(attributes)
-  Example.create!(content: "Equities are still driven by a strong tech rally and the S&P 500 hit several all-time highs along the way. China had a positive month breaking out of a year-long negative trend. Bond yields picked up early in the month in the US as well as in Europe, while remaining subdued over the rest of the period. Commodities ended negative, driven by a sharp decline in natural gas prices and weaker industrial metals. The USD continued to strengthen on positive sentiment, along with cyclical currencies, such as the EUR or GBP.", template_id: Template.last.id)
-  puts "Created #{template.title}"
-end
+puts "Creating template 2 and example 2"
+
+template_2 = Template.create(title: "My anual report", description: "This is what I need to write every year", user_id: User.last.id)
+Example.create(content:"Equities are still driven by a strong tech rally and the S&P 500 hit several all-time highs along the way. China had a positive month breaking out of a year-long negative trend. Bond yields picked up early in the month in the US as well as in Europe, while remaining subdued over the rest of the period. Commodities ended negative, driven by a sharp decline in natural gas prices and weaker industrial metals. The USD continued to strengthen on positive sentiment, along with cyclical currencies, such as the EUR or GBP.", template: template_2 )
+
+puts "Creating template 3 and example 3"
+
+template_3 = Template.create(title: "My anual report", description: "This is what I need to write every year", user_id: User.last.id)
+Example.create(content:"Equities are still driven by a strong tech rally and the S&P 500 hit several all-time highs along the way. China had a positive month breaking out of a year-long negative trend. Bond yields picked up early in the month in the US as well as in Europe, while remaining subdued over the rest of the period. Commodities ended negative, driven by a sharp decline in natural gas prices and weaker industrial metals. The USD continued to strengthen on positive sentiment, along with cyclical currencies, such as the EUR or GBP.", template: template_3 )
+
+
+
+
+
+
+puts "Created #{Template.count} templates"
+
 puts "Finished!"
