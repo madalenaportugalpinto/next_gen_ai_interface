74f98bb436dec672010ea87d8feb4d05d0f41596
Madalena Pinto <madalenaportugalpinto@gmail.com>
2024-03-08 16:35:32 +0000

    fix gpt

--- app/models/example.rb
+++ app/models/example.rb
@@ -2,14 +2,14 @@ require "json"
 
 class Example < ApplicationRecord
   belongs_to :template
-  has_many :example_fields, dependent: :destroy
+  has_many :example_fields
   after_create :generate_example_field
 
   def generate_example_field
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "given #{content}, give me the key value pairs of it in a json with the name example_field"}]
+      messages: [{ role: "user", content: "given #{content}, give me the key value pairs of the information keywords in it, in a json with the name example_field"}]
     })
     json_string = chaptgpt_response["choices"][0]["message"]["content"]
     ruby_object = JSON.parse(json_string)
@@ -23,9 +23,9 @@ class Example < ApplicationRecord
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "#{content}, give me a full text with a descriptive key value pairs of #{ruby_object["example_field"].keys}} that can change including the recipient where in the text the values are replaced by the keys like <key>"}]
-      })
-      output_from_api = chaptgpt_response["choices"][0]["message"]["content"]
+      messages: [{ role: "user", content: "#{content}, replace in this text each of #{ruby_object["example_field"].keys} in the corresponding place like this <key>"}]
+    })
+    output_from_api = chaptgpt_response["choices"][0]["message"]["content"]
     update(content: output_from_api)
   end
 end
