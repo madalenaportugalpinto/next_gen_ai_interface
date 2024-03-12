5ac4b1e19ad2ba2b1dda6a2a3d628044566068ce
Renato Schneeberger <renato.schneeberger@iam-lab.ch>
2024-03-12 15:38:48 +0000

    implemented adding/removing example fields with styling of the text

--- app/javascript/controllers/checkbox_edit_controller.js
+++ app/javascript/controllers/checkbox_edit_controller.js
@@ -2,26 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="checkbox-edit"
 export default class extends Controller {
-  static targets = ["field", "check"]
+  static targets = ["field", "check", "content"]
+
+  toggleFunction(field){
+    const prevValue = field.dataset.value;
+    field.dataset.value = field.innerHTML;
+    field.innerHTML = prevValue;
+    field.classList.toggle("field-tag")
+  }
 
   connect() {
-     console.log(this.checkTargets)
-    // let boxes = document.querySelectorAll('input[type=checkbox]')
-    // boxes.forEach((box) => {
+    this.checkTargets.forEach((check) => {
+      const fieldId = check.dataset.fieldId;
 
-    //   box.checked = true;
-    // });
+      if (!check.checked) {
+        const field = this.fieldTargets.find((field) => field.dataset.id === check.dataset.fieldId)
+        this.toggleFunction(field);
+      }
+    });
+    this.contentTarget.classList.remove("opacity-0");
   }
 
   toggleField(event) {
     const fieldId = event.target.dataset.fieldId;
+
     this.fieldTargets.forEach((field) => {
       if (field.dataset.id === fieldId) {
-        const prevValue = field.dataset.value;
-        field.dataset.value = field.innerHTML;
-        field.innerHTML = prevValue;
-        field.classList.toggle("field-tag")
+        this.toggleFunction(field)
       }
     });
   }
+
 }
